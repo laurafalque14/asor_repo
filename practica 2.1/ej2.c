7b2cecf73526282794cc5669724d8bd7eb5a897c
Laura González Falque <76649387+laurafalque14@users.noreply.github.com>
2021-10-25 09:22:14 +0200

    Create ej2.c

---
+++ practica 2.1/ej2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(){
+char *s;
+if(setuid(0) == -1)
+   printf("Error:(%d):%s\n", errno, strerror(erno));
+return 1;
+}
